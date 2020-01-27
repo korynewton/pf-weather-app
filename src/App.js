1fb245c5876704890ffe90c11e967ce93fd69d40
Kory Newton <korybnewton@gmail.com>
2020-01-27 07:49:15 -0800

    basic polling architecture to fetch data and update state every minute

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <div className="App">App rendering</div>;
+class App extends React.Component {
+  state = {
+    isPolling: false
+  };
+
+  componentDidMount() {
+    const { isPolling } = this.state;
+    if (isPolling) {
+      this.pollingMechanism = setInterval(() => this.getItems(), 1000 * 60);
+    }
+  }
+
+  componentWillUnmount() {
+    // clear pollingMechanism when unmounted
+    this.pollingMechanism = null;
+  }
+
+  updateData() {
+    console.log('updating data');
+    // todo:
+    // fetch weather at darksky endpoint
+    // update state
+  }
+
+  render() {
+    return <h1>hello world</h1>;
+  }
 }
 
 export default App;
